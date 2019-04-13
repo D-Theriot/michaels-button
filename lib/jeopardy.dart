3b32da1cfa55e142db5116fb1f6822c8f14a0c25
Dylan Theriot <dylantheriot@gmail.com>
2019-04-12 22:15:24 -0500

    localized image file, added comments

--- lib/jeopardy.dart
+++ lib/jeopardy.dart
@@ -9,11 +9,10 @@ class Jeopardy extends StatefulWidget {
 
 
 class JeopardyState extends State<Jeopardy> {
-
+  // open Audio players
   AudioPlayer advancedPlayer;
   AudioCache audioCache;
 
-  double value = 0;
   @override
   void initState(){
     super.initState();
@@ -21,36 +20,36 @@ class JeopardyState extends State<Jeopardy> {
   }
 
   void initPlayer(){
+    // create new object of audio players
     advancedPlayer = new AudioPlayer();
     audioCache = new AudioCache(fixedPlayer: advancedPlayer);
   }
   @override
   Widget build(BuildContext context) {
+    // Container is background
     return Container( 
       color: Colors.orange,
+      // Center to center
       child: Center(
+        // Column to hold the multiple buttons
         child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              // First is the image in a container
               Container(
-                height: 200.0,
-                width: 200.0,
-                decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  image: DecorationImage(
-                    fit: BoxFit.fill,
-                    image: NetworkImage("http://people.tamu.edu/~michael.nowak/imgs/pic1_cbw.jpg")
-                  ),
-                ),
+                height: 150.0,
+                width: 150.0,
+                child: Image.asset('assets/michael.jpg')
               ),
-              Padding(padding: EdgeInsets.all(10.0)),
+              Padding(padding: EdgeInsets.all(5.0)),
+              // Second is the play button
               Container(
                 child: ButtonTheme(
                 minWidth: 300.0,
                 height: 300.0,
                 buttonColor: Colors.white,
                 child: RaisedButton(
-                onPressed: () => audioCache.play('jeopardySong.mp3'), // :  () => advancedPlayer.stop()),
+                onPressed: () => audioCache.play('jeopardySong.mp3'), // Find way to play/stop w/ one button?
                 child: Text(
                     "PLAY", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 100.0)
                     )
@@ -58,6 +57,7 @@ class JeopardyState extends State<Jeopardy> {
                 )
               ),
               Padding(padding: EdgeInsets.all(5.0)),
+              // Third is stop button
               Container(
                 child: ButtonTheme(
                   minWidth: 100.0,
@@ -72,6 +72,7 @@ class JeopardyState extends State<Jeopardy> {
                 )
               ),
               Padding(padding: EdgeInsets.all(5.0)),
+              // Fourth is copyright
               Text("Made by Dylan T.", style: TextStyle(fontSize: 10.0, color: Colors.blue)),
             ],
         ),
